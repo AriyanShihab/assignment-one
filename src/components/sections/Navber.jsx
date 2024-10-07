304a9c79c05d3a9249760003d1911422b87fbca6
Ariyan Shihab <devshihab111@gmail.com>
2024-10-07 19:42:10 +0600

    fix navber responsive issue

--- src/components/sections/Navber.jsx
+++ src/components/sections/Navber.jsx
@@ -18,21 +18,20 @@ export default function Navber() {
                 className="header-logo h-12"
               />
             </a>
-      
 
             <HorizontalMenu items={itemsForHorizontalMenu}></HorizontalMenu>
 
             <div className="sm:flex">
               <a
                 href="signin.html"
-                className="flex-1 loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70"
+                className="block whitespace-nowrap sm:inline-block flex-1 loginBtn min-w-[120px] px-[22px] py-2 text-base font-medium text-white hover:opacity-70"
               >
                 Sign In
               </a>
-              
+
               <a
                 href="signup.html"
-                className="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-gray-800"
+                className="signUpBtn whitespace-nowrap min-w-[120px] rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-gray-800"
               >
                 Sign Up
               </a>
